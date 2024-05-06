3e25d1e8594c9c25c683e90a69eac530e4f4d2d5
erikajledesma <erika.jasmine06@gmail.com>
2024-05-05 18:42:05 -0700

    display weekly schedule of given driver and date

--- lab4/src/PostgreSQLExample.java
+++ lab4/src/PostgreSQLExample.java
@@ -28,6 +28,8 @@ public class PostgreSQLExample {
             //Main Menu
             System.out.println("--- POMONA TRANSIT SYSTEM ---");
             System.out.println("1: Display schedule for a trip");
+            System.out.println("3: Display the stops for a trip");
+            System.out.println("4: View a driver's weekly schedule");
             System.out.println("0: Exit the menu");
 
             //get user's menu choice
@@ -54,6 +56,60 @@ public class PostgreSQLExample {
                 // System.out.println("Date           Scheduled Start Time      Scheduled Arrival Time      Driver Name        BusID");
                 while (resultSet.next()) {
                     System.out.printf("| %-10s | %-20s | %-22s | %-11s | %-8s | %n", resultSet.getDate("Date"), resultSet.getTime("ScheduledStartTime"), resultSet.getTime("ScheduledArrivalTime"), resultSet.getString("DriverName"), resultSet.getLong("BusID"));
+                }
+                System.out.printf("----------------------------------------------------------------------------------%n");
+
+                statement.close();
+                resultSet.close();
+            }
+
+            if (menu_choice == 3){
+                //Display all stops for a given trip
+                Scanner scn3 = new Scanner(System.in);
+                System.out.println("Select your start --> end destination to see the stops for this trip: ");
+                System.out.println("1: City A --> City B");
+                System.out.println("2: City B --> City C");
+                System.out.println("3: Sin City --> Cos City");
+                System.out.println("4: Havenbrook --> Willow Springs");
+                System.out.println("5: Brooksville --> Clearwater");
+
+                int tripNum = scn3.nextInt();
+
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT S1.StopNumber, S2.StopAddress FROM TripStopInfo S1, Stop S2 WHERE S1.StopNumber = S2.StopNumber AND S1.TripNumber = " + tripNum);
+
+                System.out.printf("----------------------------------------------------------------------------------%n");
+                System.out.printf("| %-10s | %-8s | %n", "Stop Number", "Stop Address");
+                while (resultSet.next()) {
+                    System.out.printf("| %-10s | %-8s | %n", resultSet.getLong("stopnumber"), resultSet.getString("stopaddress"));
+                }
+                System.out.printf("----------------------------------------------------------------------------------%n");
+
+                statement.close();
+                resultSet.close();
+            }
+
+            if (menu_choice == 4){
+                System.out.println("Enter the name of the driver whose schedule you would like to view: ");
+                Scanner scn4 = new Scanner(System.in);
+                System.out.println("John Doe");
+                System.out.println("Jane Smith");
+                System.out.println("Anthony Orlando");
+                System.out.println("Jungkook Jeon");
+
+                String driverName = scn4.nextLine();
+
+                System.out.println("Enter in YYYY-MM-DD format what date you want to view:");
+                String driverDate = scn4.nextLine();
+
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT TripNumber, ScheduledStartTime, ScheduledArrivalTime, BusID FROM TripOffering WHERE DriverName = '" + driverName + "' AND Date = '" + driverDate + "'");
+
+                System.out.printf("----------------------------------------------------------------------------------%n");
+                System.out.printf("| %-10s | %n", "Weekly Schedule for " + driverName);
+                System.out.printf("| %-10s | %-8s | %-8s | %-8s | %n", "Trip Number", "Scheduled Start Time", "Scheduled Arrival Time", "Bus ID");
+                while (resultSet.next()) {
+                    System.out.printf("| %-10s | %-8s | %-8s | %-8s | %n", resultSet.getLong("tripnumber"), resultSet.getTime("scheduledstarttime"), resultSet.getTime("scheduledarrivaltime"), resultSet.getLong("busid"));
                 }
                 System.out.printf("----------------------------------------------------------------------------------%n");
 
