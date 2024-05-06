adaa27bab430307386c260c6b1f627a4f60d1c96
erikajledesma <erika.jasmine06@gmail.com>
2024-05-05 17:25:47 -0700

    display all trips given trip, formatted output

--- lab4/src/PostgreSQLExample.java
+++ lab4/src/PostgreSQLExample.java
@@ -37,13 +37,26 @@ public class PostgreSQLExample {
 
             if (menu_choice == 1){
                 //Display schedule of all trips for a given StartLocationName and DestinationName, and Date
+                Scanner scn1 = new Scanner(System.in);
+                System.out.println("Select your start --> end destination: ");
+                System.out.println("1: City A --> City B");
+                System.out.println("2: City B --> City C");
+                System.out.println("3: Sin City --> Cos City");
+                System.out.println("4: Havenbrook --> Willow Springs");
+                System.out.println("5: Brooksville --> Clearwater");
+
+                int tripNum = scn1.nextInt();
                 Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM Bus");
-                
+                ResultSet resultSet = statement.executeQuery("SELECT Date, ScheduledStartTime, ScheduledArrivalTime, DriverName, BusID FROM TripOffering WHERE tripnumber=" + tripNum);
+
+                System.out.printf("----------------------------------------------------------------------------------%n");
+                System.out.printf("| %-10s | %-8s | %-8s | %-15s | %-8s | %n", "Date", "Scheduled Start Time", "Scheduled Arrival Time", "Driver Name", "BusID");
+                // System.out.println("Date           Scheduled Start Time      Scheduled Arrival Time      Driver Name        BusID");
                 while (resultSet.next()) {
-                    // String columnValue = resultSet.getString("BusID");
-                    System.out.printf("id: %d model: %s year: %d \n", resultSet.getLong("busid"), resultSet.getString("model"), resultSet.getLong("year"));
+                    System.out.printf("| %-10s | %-20s | %-22s | %-11s | %-8s | %n", resultSet.getDate("Date"), resultSet.getTime("ScheduledStartTime"), resultSet.getTime("ScheduledArrivalTime"), resultSet.getString("DriverName"), resultSet.getLong("BusID"));
                 }
+                System.out.printf("----------------------------------------------------------------------------------%n");
+
                 statement.close();
                 resultSet.close();
             }
